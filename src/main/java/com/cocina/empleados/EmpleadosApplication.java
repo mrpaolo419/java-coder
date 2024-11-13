d10a416a7136b4d4e93bcc79e264425cf7a52d99
paolo coca <mrpaolo419@gmail.com>
2024-11-13 15:16:19 -0300

    cambio

--- src/main/java/com/cocina/empleados/EmpleadosApplication.java
+++ src/main/java/com/cocina/empleados/EmpleadosApplication.java
@@ -10,7 +10,7 @@ public class EmpleadosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmpleadosApplication.class, args);
-		System.out.println("putas");
+		System.out.println("ok");
     }
 }
 
